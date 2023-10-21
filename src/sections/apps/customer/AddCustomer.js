55709a98776c3a12312b265393d191fb2642f1e6
Sithum Basnayaka <sithum.dashantha@gmail.com>
2023-10-21 22:59:42 +0530

    create customer API integration

--- src/sections/apps/customer/AddCustomer.js
+++ src/sections/apps/customer/AddCustomer.js
@@ -45,6 +45,7 @@ import { openSnackbar } from 'store/reducers/snackbar';
 
 // assets
 import { CameraOutlined, DeleteFilled } from '@ant-design/icons';
+import axios from 'utils/axios';
 
 const avatarImage = require.context('assets/images/users', true);
 
@@ -53,8 +54,9 @@ const getInitialValues = (customer) => {
   const newCustomer = {
     name: '',
     email: '',
-    location: '',
-    orderStatus: ''
+    phone: '',
+    address: '',
+    country: '',
   };
 
   if (customer) {
@@ -92,22 +94,22 @@ const AddCustomer = ({ customer, onCancel }) => {
 
   const CustomerSchema = Yup.object().shape({
     name: Yup.string().max(255).required('Name is required'),
-    orderStatus: Yup.string().required('Status is required'),
     email: Yup.string().max(255).required('Email is required').email('Must be a valid email'),
-    location: Yup.string().max(500)
+    phone: Yup.string().matches(/^\(?(?:(?:0(?:0|11)\)?[\s-]?\(?|\+)44\)?[\s-]?\(?(?:0\)?[\s-]?\(?)?|0)(?:\d{5}\)?[\s-]?\d{4,5}|\d{4}\)?[\s-]?(?:\d{5}|\d{3}[\s-]?\d{3})|\d{3}\)?[\s-]?\d{3}[\s-]?\d{3,4}|\d{2}\)?[\s-]?\d{4}[\s-]?\d{4}|8(?:00[\s-]?11[\s-]?11|45[\s-]?46[\s-]?4\d))(?:(?:[\s-]?(?:x|ext\.?\s?|\#)\d+)?)$/).required("Phone number is required"),
+    age: Yup.number().min(16).required('Age is required'), // TODO: Define Min and Max age limits
+    address: Yup.string().max(255).required('Address is required'),
+    country: Yup.string().max(255).required('Country is required'),
+    zipCode: Yup.number().required('Zip code is required'),
+    web: Yup.string().matches(/^((https?|ftp):\/\/)?(www.)?(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i, "Invalid URL").required('Zip code is required'),
+    description: Yup.string().max(500).optional(),
   });
 
   const formik = useFormik({
     initialValues: getInitialValues(customer),
     validationSchema: CustomerSchema,
-    onSubmit: (values, { setSubmitting }) => {
+    onSubmit: async (values, { setSubmitting }) => {
+      console.log(values);
       try {
-        // const newCustomer = {
-        //   name: values.name,
-        //   email: values.email,
-        //   location: values.location,
-        //   orderStatus: values.orderStatus
-        // };
 
         if (customer) {
           // dispatch(updateCustomer(customer.id, newCustomer)); - update
@@ -123,11 +125,20 @@ const AddCustomer = ({ customer, onCancel }) => {
             })
           );
         } else {
+
+          const response = await axios.post('/api/v1/customer', {
+            ...values,
+            phone: Number(values.phone),
+            age: Number(values.age),
+            // TODO: set uploaded image,
+            imageUrl: "https://uploads-ssl.webflow.com/63f46d18f2e566716e8d3a69/63f88caea3a61965fef45229_oguz-yagiz-kara-MZf0mI14RI0-unsplash%20(1)-p-500.jpg"
+          });
+
           // dispatch(createCustomer(newCustomer)); - add
           dispatch(
             openSnackbar({
               open: true,
-              message: 'Customer add successfully.',
+              message: 'Customer crated successfully.',
               variant: 'alert',
               alert: {
                 color: 'success'
@@ -146,6 +157,8 @@ const AddCustomer = ({ customer, onCancel }) => {
   });
 
   const { errors, touched, handleSubmit, isSubmitting, getFieldProps, setFieldValue } = formik;
+
+  console.log(errors);
 
   return (
     <>
@@ -201,6 +214,7 @@ const AddCustomer = ({ customer, onCancel }) => {
                 </Grid>
                 <Grid item xs={12} md={8}>
                   <Grid container spacing={3}>
+                    {/* name */}
                     <Grid item xs={12}>
                       <Stack spacing={1.25}>
                         <InputLabel htmlFor="customer-name">Name</InputLabel>
@@ -214,6 +228,8 @@ const AddCustomer = ({ customer, onCancel }) => {
                         />
                       </Stack>
                     </Grid>
+                    {/* end of name */}
+                    {/* email */}
                     <Grid item xs={12}>
                       <Stack spacing={1.25}>
                         <InputLabel htmlFor="customer-email">Email</InputLabel>
@@ -227,7 +243,39 @@ const AddCustomer = ({ customer, onCancel }) => {
                         />
                       </Stack>
                     </Grid>
+                    {/* end of email */}
+                    {/* phone */}
                     <Grid item xs={12}>
+                      <Stack spacing={1.25}>
+                        <InputLabel htmlFor="customer-phone">Phone Number</InputLabel>
+                        <TextField
+                          fullWidth
+                          id="customer-phone"
+                          placeholder="Enter Phone Number"
+                          {...getFieldProps('phone')}
+                          error={Boolean(touched.phone && errors.phone)}
+                          helperText={touched.phone && errors.phone}
+                        />
+                      </Stack>
+                    </Grid>
+                    {/* end of phone */}
+                    {/* age */}
+                    <Grid item xs={12}>
+                      <Stack spacing={1.25}>
+                        <InputLabel htmlFor="customer-age">Age</InputLabel>
+                        <TextField
+                          fullWidth
+                          id="customer-age"
+                          placeholder="Enter Age"
+                          {...getFieldProps('age')}
+                          error={Boolean(touched.age && errors.age)}
+                          helperText={touched.age && errors.age}
+                        />
+                      </Stack>
+                    </Grid>
+                    {/* end of age */}
+                    {/* status */}
+                    {/* <Grid item xs={12}>
                       <Stack spacing={1.25}>
                         <InputLabel htmlFor="customer-orderStatus">Status</InputLabel>
                         <FormControl fullWidth>
@@ -258,23 +306,89 @@ const AddCustomer = ({ customer, onCancel }) => {
                           </FormHelperText>
                         )}
                       </Stack>
-                    </Grid>
+                    </Grid> */}
+                    {/* end of status */}
+                    {/* address */}
                     <Grid item xs={12}>
                       <Stack spacing={1.25}>
-                        <InputLabel htmlFor="customer-location">Location</InputLabel>
+                        <InputLabel htmlFor="customer-address">Address</InputLabel>
                         <TextField
                           fullWidth
-                          id="customer-location"
+                          id="customer-address"
                           multiline
                           rows={2}
-                          placeholder="Enter Location"
-                          {...getFieldProps('location')}
-                          error={Boolean(touched.location && errors.location)}
-                          helperText={touched.location && errors.location}
+                          placeholder="Enter Address"
+                          {...getFieldProps('address')}
+                          error={Boolean(touched.address && errors.address)}
+                          helperText={touched.address && errors.address}
                         />
                       </Stack>
                     </Grid>
+                    {/* end of address */}
+                    {/* country */}
                     <Grid item xs={12}>
+                      <Stack spacing={1.25}>
+                        <InputLabel htmlFor="customer-country">Country</InputLabel>
+                        <TextField
+                          fullWidth
+                          id="customer-country"
+                          placeholder="Enter Country"
+                          {...getFieldProps('country')}
+                          error={Boolean(touched.country && errors.country)}
+                          helperText={touched.country && errors.country}
+                        />
+                      </Stack>
+                    </Grid>
+                    {/* end of country */}
+                    {/* zip code */}
+                    <Grid item xs={12}>
+                      <Stack spacing={1.25}>
+                        <InputLabel htmlFor="customer-zip-code">Zip Code</InputLabel>
+                        <TextField
+                          fullWidth
+                          id="customer-zip-code"
+                          placeholder="Enter Zip Code"
+                          {...getFieldProps('zipCode')}
+                          error={Boolean(touched.zipCode && errors.zipCode)}
+                          helperText={touched.zipCode && errors.zipCode}
+                        />
+                      </Stack>
+                    </Grid>
+                    {/* end of zip code */}
+                    {/* web */}
+                    <Grid item xs={12}>
+                      <Stack spacing={1.25}>
+                        <InputLabel htmlFor="customer-web">Website</InputLabel>
+                        <TextField
+                          fullWidth
+                          id="customer-web"
+                          placeholder="Enter Website"
+                          {...getFieldProps('web')}
+                          error={Boolean(touched.web && errors.web)}
+                          helperText={touched.web && errors.web}
+                        />
+                      </Stack>
+                    </Grid>
+                    {/* end of web */}
+                    {/* description */}
+                    <Grid item xs={12}>
+                      <Stack spacing={1.25}>
+                        <InputLabel htmlFor="customer-description">Description</InputLabel>
+                        <TextField
+                          fullWidth
+                          id="customer-description"
+                          multiline
+                          rows={2}
+                          placeholder="Enter Description"
+                          {...getFieldProps('description')}
+                          error={Boolean(touched.description && errors.description)}
+                          helperText={touched.description && errors.description}
+                        />
+                      </Stack>
+                    </Grid>
+                    {/* end of description */}
+                    {/* bottom content */}
+                    {/* <Grid item xs={12}>
                       <Stack direction="row" justifyContent="space-between" alignItems="flex-start">
                         <Stack spacing={0.5}>
                           <Typography variant="subtitle1">Make Contact Info Public</Typography>
@@ -294,7 +408,8 @@ const AddCustomer = ({ customer, onCancel }) => {
                         </Stack>
                         <FormControlLabel control={<Switch sx={{ mt: 0 }} />} label="" labelPlacement="start" />
                       </Stack>
-                    </Grid>
+                    </Grid> */}
+                    {/* end of bottom content */}
                   </Grid>
                 </Grid>
               </Grid>
